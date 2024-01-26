99d33f70364293d3db7bb58d41fb455bdb4d0f44
alinadir <alinadirkhanlo.98@gmail.com>
2024-01-26 17:42:33 +0330

    create basic app

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// 1) Import the React and ReactDOM libraries.
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+// 2) Get a reference to the div with ID root.
+
+const el = document.getElementById('root');
+
+
+
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a component 
+
+function App(){
+
+    let message = 'ali';
+    if (Math.random() > 0.5) {
+        message = 'ey vaye ali'
+    }
+    return <h1>{message}</h1>
+}
+
+// 5) Show the component on the screen
+
+root.render(<App/>);
